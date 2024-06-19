db7dfd4bab8d3ad81934d00fb3e05b3e6236a1c4
Jesus Maria Villalobos <jveyes@gmail.com>
2024-06-19 09:08:15 -0500

    OK

--- Growing/settings.py
+++ Growing/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 #STATIC_URL = 'static/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'AppGrowing/static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
