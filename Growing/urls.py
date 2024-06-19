cab15862862a6d7dd39c6241b2ed2836991a3776
Jesus Maria Villalobos <jveyes@gmail.com>
2024-06-19 10:11:40 -0500

    OK

--- Growing/urls.py
+++ Growing/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 #urlpatterns = [
 #    path('admin/', admin.site.urls),
@@ -26,3 +28,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
