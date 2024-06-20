89c4880a5a562dacd0f85325b6cbf79a677b790a
Jesus Maria Villalobos <jveyes@gmail.com>
2024-06-19 20:58:14 -0500

    OK

--- Growing/settings.py
+++ Growing/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Growing.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'AppGrowing', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
